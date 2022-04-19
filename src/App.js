108d08760e26af8c33ecf6cd7ae4e71753376d31
Mike Chu <mikebackup2017@gmail.com>
2022-04-19 19:04:50 +0800

    feat: added new station button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
   return (
     <div className="App">
-      
+      <div>將軍澳線到站時間 （請選擇車站）</div>
+      <button>北角 </button> <button>鰂魚涌 </button> <button>油塘 </button> <button>調景嶺 </button> <button>將軍澳 </button> <button>坑口</button> <button>寶琳</button> <button>康城</button> 
+    
+    
+    
     </div>
   );
 }
